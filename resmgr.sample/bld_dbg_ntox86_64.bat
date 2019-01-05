78eaf615e4b2d46781262da55e39e6c44aa9a12e
Yordan Mladenov <yordanmladenov@gmail.com>
2019-01-05 13:46:17 +0200

    Add bat file for running cmake
    Signed-off-by: Yordan Mladenov <yordanmladenov@gmail.com>

---
+++ resmgr.sample/bld_dbg_ntox86_64.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir build 2> null
+rm null
+cd build
+rm -rf *
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=..\cmake\toolchains\win64_ntox86_64.cmake ..
+cd ..
